0151d076008322a594bd364891e2ff7dc930c7c5
sbkohel <kohels65@uwosh.edu>
2013-12-31 01:20:58 -0600

    100

--- uVa/100/Main.java
+++ uVa/100/Main.java
@@ -1,92 +1,50 @@
 import java.util.*;
 import java.io.*;
 
-class Main
-{
-   ArrayList<Integer> myArray = new ArrayList<Integer>(); 
-   int[][] myOtherArray;
-   int[] order;
-      
-   void readfile() throws IOException
-   {
+class Main{
+   void readfile() throws IOException{
         Scanner s = new Scanner(System.in);
-        //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int num =-1;
-        //String line;
-        //System.out.println("enter nums.  Enter 0 when done");
-        while(num !=0)
-        {
-            num =  s.nextInt();
-            //line = br.readLine();
-            //num = Integer.parseInt(line);
-            
-            myArray.add(num);
+        int firstNum = 0;
+        int secondNum = 0;
+        
+        while(s.hasNext()){
+            firstNum =  s.nextInt();
+            secondNum = s.nextInt();
+            System.out.println(firstNum + " " + secondNum + " " + findMax(firstNum, secondNum));
         }
-   }
+    }
    
-   void reset(int m)
-   {
-       int i = 0;
-       while (i < myOtherArray.length)
-       {
-           myOtherArray[i][0] = i+1;
-           myOtherArray[i][1] = 0;
-           order[i] = 0;
-           i++;
+   public int findMax(int num1, int num2){
+       int max = 0;
+       int start;
+       int difference;
+       if (num1 > num2){
+          int temp = num1;
+          num1 = num2;
+          num2 = temp;                
        }
-   }
-   
-   int find(int m)
-   {
-       myOtherArray = new int[m][2];
-       order = new int[m];
-       int leap = 4;
-       reset(m);
-      
-       while (order[m-1] != 13)
-       {
-           leap++;
-           int index = 0;
-           int counter = 1;
-           
-           reset(m);
-           myOtherArray[0][1] = 1;
-           order[0] = 1;
-           
-           while (counter != myOtherArray.length)
-           {
-              int pointer = 0; 
-              while (true)
-              {
-                  if (pointer == leap) break;
-                  index++;
-                  
-                  if (index < myOtherArray.length && myOtherArray[index][1] == 0) 
-                  {
-                    pointer++;
-                  }
-                  else if (index == myOtherArray.length) index = 0;
-              }
-              myOtherArray[index][1] =  1;
-              order[counter] =  myOtherArray[index][0];        
-              counter++;
-           }
-       }     
-       return leap;
+        difference = num2 - (num1-1);
+
+       for (int i = 0; i < difference; i++){
+          int count = 1;
+          start = num1 + i;
+             
+          while (start != 1){
+             if (start % 2 == 0) start = start / 2;
+             else start = (3 * start) + 1;
+             count++;
+          } 
+          if ((count) > max)
+             max = count;
+        
+       }
+       return max;           
    } 
            
-   public static void main(String[] args)
-   {
+   public static void main(String[] args){
        Main m = new Main();
-       try
-       {
-               m.readfile();
-               for(int i = 0; 1 < m.myArray.size();i++)
-               {
-                   int num = m.myArray.get(i);
-                   if (num != 0)
-                    System.out.printf("%d\n",m.find(num));
-               }
+       try{
+          m.readfile();               
        }
        catch(Exception e){}
    }
